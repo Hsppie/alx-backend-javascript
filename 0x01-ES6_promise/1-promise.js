b580b634def013effcb5d76a1f5978551443870a
Hsppie <wambimicheal@gmail.com>
2023-06-29 14:57:10 +0300

    ES6 promise solutions

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      // eslint-disable-next-line prefer-promise-reject-errors
+      reject({
+        error: 'The fake API is not working currently',
+      });
+    }
+  });
+}
